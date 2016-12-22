fb7a6b58d855ce72ea9e009ab472a562e301d7a8
shengsd <shengshaodong@gmail.com>
2016-12-22 14:54:26 +0800

    调整图标

--- mfc/resource.h
+++ mfc/resource.h
@@ -5,16 +5,13 @@
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
-#define IDD_MFC_DIALOG                  102
 #define IDD_CME_DIALOG                  102
 #define IDR_MAINFRAME                   128
 #define IDD_ALTER_DIALOG                131
 #define IDC_COMBO1                      1006
 #define IDC_COMBO2                      1007
 #define IDC_COMBO3                      1008
-#define IDC_BUTTON1                     1009
 #define IDC_BUTTON_ENTER                1009
-#define IDC_BUTTON2                     1010
 #define IDC_BUTTON_CLEAR                1010
 #define IDC_LIST3                       1011
 #define IDC_COMBO4                      1013
@@ -28,9 +25,7 @@
 #define IDC_LIST4                       1024
 #define IDC_LIST1                       1026
 #define IDC_BUTTON3                     1028
-#define IDC_BUTTON4                     1029
 #define IDC_BUTTON5                     1030
-#define IDC_BUTTON6                     1031
 #define IDC_EDIT3                       1032
 #define IDC_EDIT4                       1033
 #define IDC_COMBO6                      1036
@@ -44,7 +39,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1046
 #define _APS_NEXT_SYMED_VALUE           101
